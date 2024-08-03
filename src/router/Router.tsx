e05b75e40c605dae6c4c52581d7f9761e336c5d5
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-03 22:02:15 +0300

    feat/set up router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,57 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Outlet, redirect, RouterProvider} from "react-router-dom";
 
 import NotFound from "../pages/NotFound/NotFound";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import Home from "../features/auth/pages/home/Home";
+import Login from "../features/auth/pages/login/Login";
+import SignUp from "../features/auth/pages/signup/SignUp";
+import CreateTeam from "../features/createTeam/pages/CreateTeam";
+import FindTeam from "../features/findTeam/pages/FindTeam";
+import FindPartner from "../features/findPartner/pages/FindPartner";
+
+const auth = false; // come from authslice
 
 const Router = () => {
 	return (
 		<RouterProvider
 			router={createBrowserRouter([
-				{path: "", element: <h1>Scrow Code</h1>},
+				{
+					path: "",
+					element: (
+						<>
+							<Header />
+							<main>
+								<Outlet />
+							</main>
+							<Footer />
+						</>
+					),
+					children: [
+						{index: true, element: <Home />},
+						{
+							path: "CreateTeam",
+							element: <CreateTeam />,
+							loader: async () => (auth ? <CreateTeam /> : redirect("/login")),
+						},
+						{
+							path: "FindTeam",
+							element: <FindTeam />,
+							loader: async () => (auth ? <FindTeam /> : redirect("/login")),
+							// will have a children array
+						},
+						{
+							path: "FindPartner",
+							element: <FindPartner />,
+							loader: async () => (auth ? <FindPartner /> : redirect("/login")),
+							// will have a children array
+						},
+					],
+				},
+				{path: "login", element: <Login />},
+				{path: "signup", element: <SignUp />},
 				{path: "*", element: <NotFound />},
 			])}
 		/>
