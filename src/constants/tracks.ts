21f7565aa395847eff8bbea38376a5b3559435e6
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-07 22:43:05 +0300

    fix(auth): resolve issues with protected page access

--- src/constants/tracks.ts
+++ src/constants/tracks.ts
@@ -1,33 +1,5 @@
 const tracks = [
 	{
-		_id: "66b01be4d3e25dc283ef24b2",
-		name: "Other",
-		slug: "other",
-		createdAt: "2024-08-05T00:25:08.306Z",
-		updatedAt: "2024-08-05T00:27:38.359Z",
-	},
-	{
-		_id: "66b01bd1d3e25dc283ef24af",
-		name: "Mobile devloper",
-		slug: "mobile-devloper",
-		createdAt: "2024-08-05T00:24:49.324Z",
-		updatedAt: "2024-08-05T00:24:49.324Z",
-	},
-	{
-		_id: "66b01ba4d3e25dc283ef24ac",
-		name: "Ui Ux",
-		slug: "ui-ux",
-		createdAt: "2024-08-05T00:24:04.618Z",
-		updatedAt: "2024-08-05T00:24:04.618Z",
-	},
-	{
-		_id: "66b01b8bd3e25dc283ef24a9",
-		name: "Ai",
-		slug: "ai",
-		createdAt: "2024-08-05T00:23:39.823Z",
-		updatedAt: "2024-08-05T00:23:39.823Z",
-	},
-	{
 		_id: "66b01b79d3e25dc283ef24a6",
 		name: "front end",
 		slug: "front-end",
@@ -40,6 +12,34 @@ const tracks = [
 		slug: "back-end",
 		createdAt: "2024-08-05T00:22:27.565Z",
 		updatedAt: "2024-08-05T00:22:27.565Z",
+	},
+	{
+		_id: "66b01ba4d3e25dc283ef24ac",
+		name: "Ui Ux",
+		slug: "ui-ux",
+		createdAt: "2024-08-05T00:24:04.618Z",
+		updatedAt: "2024-08-05T00:24:04.618Z",
+	},
+	{
+		_id: "66b01bd1d3e25dc283ef24af",
+		name: "Mobile devloper",
+		slug: "mobile-devloper",
+		createdAt: "2024-08-05T00:24:49.324Z",
+		updatedAt: "2024-08-05T00:24:49.324Z",
+	},
+	{
+		_id: "66b01b8bd3e25dc283ef24a9",
+		name: "Ai",
+		slug: "ai",
+		createdAt: "2024-08-05T00:23:39.823Z",
+		updatedAt: "2024-08-05T00:23:39.823Z",
+	},
+	{
+		_id: "66b01be4d3e25dc283ef24b2",
+		name: "Other",
+		slug: "other",
+		createdAt: "2024-08-05T00:25:08.306Z",
+		updatedAt: "2024-08-05T00:27:38.359Z",
 	},
 ];
 
