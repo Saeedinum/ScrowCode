a9e39300c2a158b09bcaf14b527fabda253c181b
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-07 23:31:15 +0300

    fix: show tracks based on the constant diredctory

--- src/features/createTeam/pages/CreateTeam.tsx
+++ src/features/createTeam/pages/CreateTeam.tsx
@@ -1,13 +1,15 @@
-import {useForm, SubmitHandler} from "react-hook-form";
+import {useState} from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import {TCreateTeamData} from "../../../types";
-import { useState } from "react"
+import {useAppSelector} from "../../../store/hooks";
+import tracks from "../../../constants/tracks";
 
 const CreateTeam = () => {
-	console.log(" - - Render - - ");
-	const user = "moahemd" // get from authslice
-	const [teamMembers , setTeamMembers] = useState<string[]>([]);
+	const user = useAppSelector((state) => state.auth.user);
+	const [teamMembers, setTeamMembers] = useState<string[]>([]);
 
-	const {register, handleSubmit} = useForm<TCreateTeamData>();
+	const {register, handleSubmit, getValues, watch} = useForm<TCreateTeamData>();
 
 	const onSubmit: SubmitHandler<TCreateTeamData> = (data) => console.log(data);
 
@@ -24,28 +26,12 @@ const CreateTeam = () => {
 			<div>
 				<h2>Requirements</h2>
 				<div>
-					<input type='checkbox' id='check1' value='Backend' {...register("requirement")} />
-					<label htmlFor='check1'>Backend</label>
-				</div>
-				<div>
-					<input type='checkbox' id='check2' value='Frontend' {...register("requirement")} />
-					<label htmlFor='check2'>Frontend</label>
-				</div>
-				<div>
-					<input type='checkbox' id='check3' value='Mobile' {...register("requirement")} />
-					<label htmlFor='check3'>Mobile</label>
-				</div>
-				<div>
-					<input type='checkbox' id='check4' value='UI/UX' {...register("requirement")} />
-					<label htmlFor='check4'>UI/UX</label>
-				</div>
-				<div>
-					<input type='checkbox' id='check5' value='AI' {...register("requirement")} />
-					<label htmlFor='check5'>AI</label>
-				</div>
-				<div>
-					<input type='checkbox' id='check6' value='Other' {...register("requirement")} />
-					<label htmlFor='check6'>Other</label>
+					{tracks.map((track) => (
+						<div key={track._id}>
+							<input {...register("requirement")} type='checkbox' id={track.slug} value={track.slug} />
+							<label htmlFor={track.slug}> {track.name}</label>
+						</div>
+					))}
 				</div>
 			</div>
 			<div>
@@ -58,16 +44,15 @@ const CreateTeam = () => {
 				<h2>Add memebers</h2>
 				{/* add user as default team leader */}
 				{/* button to add  */}
-					<div >
-						<input type='text' {...register("userName")} value={`${user}`} />
-				{teamMembers.map((member, index) => (
-						<input key={index} type='text' {...register("userName")} value={`${member ?? ""}`} />
-				))}
-					</div>
-				<button onClick={() => (
-					
-					setTeamMembers([...teamMembers, ""])
-				)}>Add Member</button>
+				<div>
+					<input type='text' value={`${user?.fullName}`} disabled />
+					{teamMembers.map((member, index) => (
+						<input key={index} type='text' {...register(`userName.${index}`)} />
+					))}
+				</div>
+				<button onClick={() => setTeamMembers([...teamMembers, ""])} disabled={teamMembers.length >= watch("members")}>
+					Add Member
+				</button>
 			</div>
 			<button type='submit'>Create Team</button>
 		</form>
