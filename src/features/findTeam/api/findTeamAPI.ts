501a0c38cc37790cb18501947a0477061bf90b30
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-10 23:13:04 +0300

    feat(API): create fetchTeams endpoint

--- src/features/findTeam/api/findTeamAPI.ts
+++ src/features/findTeam/api/findTeamAPI.ts
@@ -2,7 +2,17 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const findTeamAPI = createApi({
 	baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_API_BASE_URL}),
-	endpoints: () => ({}),
+	endpoints: (builder) => ({
+		fetchTeams: builder.query({
+			query: (token) => ({
+				url: `team`,
+				method: "GET",
+				header: {
+					authorization: token,
+				},
+			}),
+		}),
+	}),
 });
 
-// export const { } = findTeamAPI;
+export const {useFetchTeamsQuery} = findTeamAPI;
--- src/features/findTeam/pages/FindTeam.tsx
+++ src/features/findTeam/pages/FindTeam.tsx
@@ -1,4 +1,12 @@
+import { useAppSelector } from "../../../store/hooks"
+import { useFetchTeamsQuery } from "../api/findTeamAPI";
+
 const FindTeam = () => {
+	const token = useAppSelector((state) => state.auth.user?.token);
+
+	const { data, error, isLoading } = useFetchTeamsQuery(token);
+	console.log(data)
+
 	return <div>findTeam</div>;
 };
 
