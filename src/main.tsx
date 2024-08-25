5aa1f17871f7cd0a9f339da36dbff0ad006be884
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-25 08:26:45 +0300

    style: enable darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,14 @@ import Router from "./router/Router";
 import {store} from "./store/store";
 import {Provider} from "react-redux";
 
+import {ThemeProvider} from "@/components/theme-provider";
+
 // import "../src/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<Provider store={store}>
-		<Router />
+		<ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+			<Router />
+		</ThemeProvider>
 	</Provider>,
 );
