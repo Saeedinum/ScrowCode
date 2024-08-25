ab72f4aa334f76d1ed06272d7702a265a1394a63
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-25 08:03:51 +0300

    feat(teams): fetch teams

--- src/features/findTeam/api/findTeamAPI.ts
+++ src/features/findTeam/api/findTeamAPI.ts
@@ -1,13 +1,15 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const findTeamAPI = createApi({
+	reducerPath: "findTeamAPI",
 	baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_API_BASE_URL}),
+	tagTypes: ["Team"],
 	endpoints: (builder) => ({
 		fetchTeams: builder.query({
-			query: (token) => ({
-				url: `team`,
+			query: ({token}) => ({
+				url: "team",
 				method: "GET",
-				header: {
+				headers: {
 					authorization: token,
 				},
 			}),
