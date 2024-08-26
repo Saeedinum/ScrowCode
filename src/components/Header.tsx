03838ba8c6609f6701410e9d5528313b5b1539cc
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-26 09:48:07 +0300

    style: add header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,60 @@
 import {NavLink} from "react-router-dom";
 
-const auth = true;
+import Logo from "/src/assets/logo.svg";
+
+import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+
+const auth = false;
 
 const Header = () => {
 	return (
-		<>
-			<header>
-				<nav>
-					<NavLink to={""}>home</NavLink>
-					<NavLink to={"about"}>about</NavLink>
-					<NavLink to={"FindTeam"}>joinTeam</NavLink>
-					<NavLink to={"createTeam"}>createTeam</NavLink>
-					<NavLink to={"findPartner"}>findPartner</NavLink>
+		<header className='flex  justify-center items-center gap-60 py-3 text-primary-first text-base font-bold bg-white drop-shadow-lg shadow-black z-50 sticky top-0 '>
+			<img src={Logo} alt='logo' />
 
+			<nav className='flex gap-5 items-center text-primary-first font-bold '>
+				<NavLink to={""} className='hover:text-primary-third outline-none'>
+					home
+				</NavLink>
+				<NavLink to={"about"} className='hover:text-primary-third'>
+					about
+				</NavLink>
 
-					{auth ? (
-						<>
-							<NavLink to={"Notifications"}>Notifications</NavLink>
-							<NavLink to={"profile"}>profile</NavLink>
-						</>
-					) : (
-						<>
-							<NavLink to={"login"}>login</NavLink>
-							<NavLink to={"signup"}>signup</NavLink>
-						</>
-					)}
-				</nav>
-			</header>
-		</>
+				<DropdownMenu>
+					<DropdownMenuTrigger className='outline-none'>Teams</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuItem>
+							<NavLink to={"FindTeam"}>join Team</NavLink>
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<NavLink to={"createTeam"}>create Team</NavLink>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+
+				<NavLink to={"findPartner"} className='hover:text-primary-third'>
+					find Partner
+				</NavLink>
+			</nav>
+
+			<div>
+				{auth ? (
+					<div>
+						<NavLink to={"Notifications"}>Notifications</NavLink>
+						<NavLink to={"profile"}>profile</NavLink>
+					</div>
+				) : (
+					<div className='flex items-center gap-5 '>
+						<NavLink to={"login"} className={"hover:text-primary-third"}>
+							Log in
+						</NavLink>
+						<NavLink to={"signup"} className='bg-primary-first text-primary-fourth rounded-[12px] px-[12px] py-1 '>
+							Sign Up
+						</NavLink>
+					</div>
+				)}
+			</div>
+		</header>
 	);
 };
 
