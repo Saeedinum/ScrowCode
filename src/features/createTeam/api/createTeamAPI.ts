b12402d3a5cd86a490088d0d26cf37b5e23b5ec4
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-31 18:27:16 +0300

    fix: update createTeam API

--- src/features/createTeam/api/createTeamAPI.ts
+++ src/features/createTeam/api/createTeamAPI.ts
@@ -1,27 +1,30 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {TCreateTeamData} from "../../../types";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { TCreateTeamData } from "../../../types";
+import { Ttracks } from "@/types/auth";
 
 export const createTeamAPI = createApi({
-	baseQuery: fetchBaseQuery({
-		baseUrl: import.meta.env.VITE_API_BASE_URL,
-	}),
-	endpoints: (builder) => ({
-		createTeam: builder.mutation<void, TCreateTeamData>({
-			query: (data) => ({
-				url: "team",
-				method: "POST",
-				body: {
-					teamName: data.teamName,
-					projectIdea: data.projectIdea,
-					userName: data.userName.map((user) => user.name),
-					requirement: data.requirement,
-				},
-				headers: {
-					authentication: data.token,
-				},
-			}),
-		}),
-	}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_BASE_URL,
+  }),
+  endpoints: (builder) => ({
+    createTeam: builder.mutation<
+      void,
+      { data: TCreateTeamData; token: string }
+    >({
+      query: ({ data, token }) => ({
+        url: "team",
+        method: "POST",
+        body: data,
+        headers: {
+          Authorization: token,
+        },
+      }),
+    }),
+    getTracks: builder.query<Ttracks[], void>({
+      query: () => "track",
+      transformResponse: (response: { data: Ttracks[] }) => response.data,
+    }),
+  }),
 });
 
-export const {useCreateTeamMutation} = createTeamAPI;
+export const { useCreateTeamMutation, useGetTracksQuery } = createTeamAPI;
