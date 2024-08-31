d96da2d4f498a2fb31b0464be04e283aa0080987
Saeedinum <mo7amed0ptr@gmail.com>
2024-08-31 12:10:31 +0300

    chore(auth): configure oauth/google

---
+++ src/config/Oauth.ts
@@ -0,0 +1,14 @@
+export const Oauth = {
+  client_id: import.meta.env.VITE_OAUTH_CLIENT_ID,
+  project_id: import.meta.env.VITE_OAUTH_REDIRECT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_secret: import.meta.env.VITE_OAUTH_CLIENT_SECRET,
+  redirect_uris: ["http://localhost:5173/"],
+  javascript_origins: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost",
+  ],
+};
